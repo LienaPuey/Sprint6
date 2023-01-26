b4cfea1339986e96b51a74967f7f9641a58a5a5b
Billy Everyteen <billyeveryteen@example.com>
2023-01-26 12:49:31 +0100

    first steps

--- sprint6_angular/src/app/escena/escena.component.ts
+++ sprint6_angular/src/app/escena/escena.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-escena',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./escena.component.scss']
 })
 export class EscenaComponent {
-
+  @Input() data:string[]= [];
+ 
 }
--- sprint6_angular/src/app/home/home.component.ts
+++ sprint6_angular/src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import * as frases from '../../app/frases.json';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  frases = frases;
 }
