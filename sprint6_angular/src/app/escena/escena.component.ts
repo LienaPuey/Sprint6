c9aeefc8397445018fa15ba4bcd2fa94c846a58f
unknown <lienapuey@gmail.com>
2023-01-28 21:22:37 +0100

    nivel 1

--- sprint6_angular/src/app/escena/escena.component.ts
+++ sprint6_angular/src/app/escena/escena.component.ts
@@ -7,6 +7,17 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./escena.component.scss']
 })
 export class EscenaComponent {
-  @Input() data:string[]= [];
- 
+  @Input() data: { frases: any[] }={ frases: [] };
+
+  selected: number = 0;
+  prev(){
+    if (this.selected > 0) {
+      this.selected--;
+   }
+  }
+  next(){
+    if (this.selected < this.data.frases.length - 1) {
+      this.selected++;
+  }
+  }
 }
--- sprint6_angular/src/app/home/home.component.ts
+++ sprint6_angular/src/app/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import * as frases from '../../app/frases.json';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent {  
   frases = frases;
+  
+  
 }
